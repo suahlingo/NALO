f5750acfa12ce65d76864a8c5ea4846a772f7b23
soapcookie <sgua0720@gmail.com>
2024-08-27 21:59:05 +0900

    initial commit

---
+++ src/main/java/com/example/NALO/NaloApplication.java
@@ -0,0 +1,13 @@
+package com.example.NALO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaloApplication.class, args);
+	}
+
+}
